93adafd2684c72a1340d694cd456f9696789e60e
unknown <13776250336@163.com>
2021-01-06 23:11:40 +0800

    2021-1-6

---
+++ 1-6/scanf的用法.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    scanf("%d", &i);
+    printf("i = %d\n",i);//&i 表示i的地址 &是一个取地址符
+    return i;
+}
---
+++ 1-6/一次给多个scanf赋值.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main(void) 
+{
+    int i , j;
+    scanf("%d %d", &i , &j);
+    printf("i = %d ,j = %d\n" , i , j);
+    return 0;
+}
